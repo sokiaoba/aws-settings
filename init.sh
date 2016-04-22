d6ca1ab8e4e821e5ea8c3f5d29939055310c3abb
sokiaoba <souki.aoba@gmail.com>
2016-04-22 09:11:45 +0000

    update

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ autoload -Uz throw
 # git
 git config --global user.name "sokiaoba"
 git config --global user.email souki.aoba@gmail.com
+git config --global core.editor "vim"
 git config --global alias.br branch
 git config --global alias.cm commit
 git config --global alias.co checkout
