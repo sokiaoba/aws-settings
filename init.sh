8df32efdeeb920ad6390c2598cca955f38ff252c
sokiaoba <souki.aoba@gmail.com>
2016-04-22 09:06:46 +0000

    update

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+#!/bin/zsh
+autoload -Uz catch
+autoload -Uz throw
+
 # git
 git config --global user.name "sokiaoba"
 git config --global user.email souki.aoba@gmail.com
@@ -14,7 +18,7 @@ git clone https://github.com/sokiaoba/zsh-settings.git
 cp zsh-settings/.zshrc ./
 
 ZSH_PATH=`which zsh`
-sudo chsh -s $ZSH_PATH
+sudo chsh ec2-user
 
 # vim
 git clone https://github.com/sokiaoba/vim-settings.git
@@ -22,8 +26,6 @@ cp vim-settings/.vimrc ./
 
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
-echo "you've finished vim settings. use :NeoBundleInstall"
 
 # python
 PIP_PATH=`which pip`
@@ -35,8 +37,19 @@ sudo $PIP_PATH install scipy
 sudo $PIP_PATH install selenium
 
 # phantomjs
-cd /usr/local/src/
-sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo tar jxf phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo ln -s phantomjs-2.1.1-linux-x86_64 phantomjs
-sudo ln -s /usr/local/src/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+PHANTOMJS_PATH="/usr/local/bin/phantomjs"
+if [ ! -f $PHANTOMJS_PATH ]; then
+	cd /usr/local/src/
+	{
+		sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 || throw "e"
+		sudo tar jxf phantomjs-2.1.1-linux-x86_64.tar.bz2 
+		sudo ln -s phantomjs-2.1.1-linux-x86_64 phantomjs
+		sudo ln -s /usr/local/src/phantomjs/bin/phantomjs $PHANTOMJS_PATH
+	} always {
+		if catch "e"; then
+			echo "you've failed to download phantomjs. do it again later."
+		elif; then
+			echo "you've finished settings. open vim and set :NeoBundleInstall"
+		fi
+	}
+fi
