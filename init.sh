675a8fce5a71fcde493babeb248342ed7bda93e2
EC2 Default User <ec2-user@ip-172-31-29-248.us-west-2.compute.internal>
2016-04-22 05:20:48 +0000

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-cd ~/
-
 # zsh
+sudo yum -y install zsh
+
+cd ~/
 git clone https://github.com/sokiaoba/zsh-settings.git
 cp zsh-settings/.zshrc ./
 
@@ -17,17 +18,17 @@ git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 echo "you've finished vim settings. use :NeoBundleInstall"
 
 # python
-sudo pip install --upgrade pip 
-sudo pip install chainer
-sudo pip install numpy 
-sudo pip install pylab
-sudo pip install scikit-learn
-sudo pip install scipy
-sudo pip install selenium
+PIP_PATH=`which pip`
+sudo $PIP_PATH install --upgrade pip 
+sudo $PIP_PATH install chainer
+sudo $PIP_PATH install numpy 
+sudo $PIP_PATH install scikit-learn
+sudo $PIP_PATH install scipy
+sudo $PIP_PATH install selenium
 
 # phantomjs
-sudo cd /usr/local/src/
+cd /usr/local/src/
 sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo tar jxf ~/phantomjs-2.1.1-linux-x86_64.tar.bz2 
+sudo tar jxf phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo ln -s phantomjs-2.1.1-linux-x86_64 phantomjs
 sudo ln -s /usr/local/src/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
