dbcf71d8f61b1e4028e5b8936bb641eba25d1879
sokiaoba <souki.aoba@gmail.com>
2016-04-26 08:12:51 +0000

    update

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ sudo yum -y install zsh
 ZSH_PATH=`which zsh`
 HERE="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+$ZSH_PATH $HERE/init_dir.sh
 $ZSH_PATH $HERE/init_zsh.sh
 $ZSH_PATH $HERE/init_git.sh
 $ZSH_PATH $HERE/init_vim.sh
---
+++ init_dir.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+sudo mkdir /proj
+sudo chown ec2-user:ec2-user /proj
